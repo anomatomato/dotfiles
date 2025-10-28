2ad7901df644d98b3455279842110fc92d70685b
An Hoang <55243106+anomatomato@users.noreply.github.com>
2025-10-28 10:28:43 +0100

    config(nvim): prettier has longer print-width of 120, so lines are not cut too short

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,15 @@
+return {
+  "stevearc/conform.nvim",
+  ---@type conform.setupOpts
+  opts = {
+    -- log_level = vim.log.levels.DEBUG,
+    formatters = {
+      prettier = {
+        append_args = {
+          "--print-width",
+          "120", -- max line length
+        },
+      },
+    },
+  },
+}
