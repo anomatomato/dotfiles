95ccfffe1396bf0fac35b7430c2035169127152f
An Hoang <55243106+anomatomato@users.noreply.github.com>
2025-11-12 23:57:32 +0100

    yazi: add plugins, like eza-preview, copy-file-contents, trash bin

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -11,3 +11,78 @@ function Linemode:size_and_mtime()
 	local size = self._file:size()
 	return string.format("%s %s", size and ya.readable_size(size) or "-", time)
 end
+
+require("eza-preview"):setup({
+	-- Set the tree preview to be default (default: true)
+	default_tree = true,
+
+	-- Directory depth level for tree preview (default: 3)
+	level = 2,
+
+	-- Show file icons
+	icons = true,
+
+	-- Follow symlinks when previewing directories (default: true)
+	follow_symlinks = true,
+
+	-- Show target file info instead of symlink info (default: false)
+	dereference = false,
+
+	-- Show hidden files (default: true)
+	all = true,
+
+	-- Ignore files matching patterns (default: {})
+	-- ignore_glob = "*.log"
+	-- ignore_glob = { "*.tmp", "node_modules", ".git", ".DS_Store" }
+	-- SEE: https://www.linuxjournal.com/content/pattern-matching-bash to learn about glob patterns
+	ignore_glob = {},
+
+	-- Ignore files mentioned in '.gitignore'  (default: true)
+	git_ignore = true,
+
+	-- Show git status (default: false)
+	git_status = false,
+})
+
+require("copy-file-contents"):setup({
+	append_char = "\n",
+	notification = true,
+})
+
+require("recycle-bin"):setup({
+	-- Optional: Override automatic trash directory discovery
+	-- trash_dir = "~/.local/share/Trash/",  -- Uncomment to use specific directory
+})
+
+require("restore"):setup({
+	-- Set the position for confirm and overwrite prompts.
+	-- Don't forget to set height: `h = xx`
+	-- https://yazi-rs.github.io/docs/plugins/utils/#ya.input
+	position = { "center", w = 70, h = 40 }, -- Optional
+
+	-- Show confirm prompt before restore.
+	-- NOTE: even if set this to false, overwrite prompt still pop up
+	show_confirm = true, -- Optional
+
+	-- Suppress success notification when all files or folder are restored.
+	suppress_success_notification = true, -- Optional
+
+	-- colors for confirm and overwrite prompts
+	theme = { -- Optional
+		-- Default using style from your flavor or theme.lua -> [confirm] -> title.
+		-- If you edit flavor or theme.lua you can add more style than just color.
+		-- Example in theme.lua -> [confirm]: title = { fg = "blue", bg = "green"  }
+		title = "blue", -- Optional. This value has higher priority than flavor/theme.lua
+
+		-- Default using style from your flavor or theme.lua -> [confirm] -> content
+		-- Sample logic as title above
+		header = "green", -- Optional. This value has higher priority than flavor/theme.lua
+
+		-- header color for overwrite prompt
+		-- Default using color "yellow"
+		header_warning = "yellow", -- Optional
+		-- Default using style from your flavor or theme.lua -> [confirm] -> list
+		-- Sample logic as title and header above
+		list_item = { odd = "blue", even = "blue" }, -- Optional. This value has higher priority than flavor/theme.lua
+	},
+})
