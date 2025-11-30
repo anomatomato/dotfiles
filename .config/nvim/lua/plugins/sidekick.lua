c82c0d3418d38ee97ced836c87ffa34d252a0e87
An Hoang <55243106+anomatomato@users.noreply.github.com>
2025-11-30 21:48:05 +0100

    nvim: disable copilot lsp by default, consumes too much RAM

--- .config/nvim/lua/plugins/sidekick.lua
+++ .config/nvim/lua/plugins/sidekick.lua
@@ -1,10 +1,31 @@
 ---@type LazySpec
 return {
   {
+    "neovim/nvim-lspconfig",
+    -- Works as a function, because it seems to run AFTER LazyVim has merged all defaults/extras and lets you overwrite the final table.
+    -- A table would just get deep-merged early. See https://www.lazyvim.org/configuration/plugins#%EF%B8%8F-customizing-plugin-specs
+    ---@param _ LazyPlugin
+    opts = function(_, opts)
+      ---@type lazyvim.lsp.Config
+      opts.servers.copilot = {
+        -- Disable copilot lsp by default to save RAM, every nvim window consumes 300-800mb RAM with enabled
+        enabled = false,
+        settings = {
+          telemetry = {
+            -- doesn't work, seems to be a vscode setting
+            telemetryLevel = "off",
+          },
+        },
+      }
+    end,
+  },
+
+  {
     "folke/sidekick.nvim",
     ---@type sidekick.Config
     opts = {
       nes = {
+        -- enabled = false,
         trigger = {
           -- events that trigger sidekick next edit suggestions
           events = { "ModeChanged i:n", "TextChanged", "User SidekickNesDone", "TextChangedI" }, --"TextChangedI"
@@ -19,7 +40,7 @@ return {
         "<tab>",
         function()
           -- if there is a next edit, jump to it, otherwise apply it if any
-          if not require("sidekick.nes").apply() then
+          if not (require("sidekick.nes").have() and require("sidekick.nes").apply()) then
             return "<Tab>" -- fallback to normal tab
           end
         end,
@@ -46,7 +67,7 @@ return {
           end,
           -- function() -- if you are using Neovim's native inline completions
           --   return vim.lsp.inline_completion.get()
-          -- end,
+          -- end  ,
           "fallback",
         },
       },
